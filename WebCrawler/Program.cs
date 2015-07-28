778d20067760b57c735b1fd208b059df17e53a81
Bilal A. Durrani <bilal.durrani@ericsson.com>
2015-07-28 14:43:34 +0100

    Skeleton - Not working

---
+++ WebCrawler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var page = new WebPage(new Uri("http://wiprodigital.com/"));
+
+            page.GetBody();
+        }
+    }
+}
---
+++ WebCrawler/WebPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+
+    class WebPage
+    {
+        private Uri address;
+
+        public WebPage(Uri address)
+        {
+            this.address = address;
+        }
+
+        public string GetBody()
+        {
+            var request = WebRequest.Create(address);
+
+            request.GetRequestStreamAsync();
+            request.Method = "GET";
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+        public string Get
+    }
+}
